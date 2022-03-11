419645d907510b19280ade9b1e00ab77591dd91f
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-11 16:54:26 +0100

    reached milestones 2 and 3

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,24 @@
 for (let i=1; i<=100; i++){
+    let container = document.getElementById("container");
+    container.innerHTML += `<div class="square"><p>${i}</p></div>`;
+    let square = document.getElementsByClassName("square");
     if (i % 3 === 0 && i % 5 === 0){
         console.log("FizzBuzz");
+        square[i-1].style.backgroundColor = "#f0466f";
+        square[i-1].innerHTML = "<p>fizzbuzz</p>"
     }
     else if (i % 3 === 0){
         console.log("Fizz");
+        square[i-1].style.backgroundColor = "#0bd6a1";
+        square[i-1].innerHTML = "<p>fizz</p>"
     }
     else if (i % 5 === 0){
         console.log("Buzz");
+        square[i-1].style.backgroundColor = "#ffd166";
+        square[i-1].innerHTML = "<p>buzz</p>"
     }
     else{
         console.log(i);
+        square[i-1].style.backgroundColor = "#1389b2";
     }
 }
